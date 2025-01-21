85f1977360c35e331fc0862c9e47d2f3d589da29
ShrikeSwallow <maciej.jaskolka@gmail.com>
2025-01-21 06:46:03 +0000

    Add fetching pokemon list to an effect

--- src/components/GameWindow.jsx
+++ src/components/GameWindow.jsx
@@ -1,34 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 console.log("GameWindow loaded");
-
-const fetchOgPokemon = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-  const ogPokemon = await response.json();
-  //console.log(ogPokemon);
-  ogPokemon.results.forEach((pokemon, index) => {
-    //fetchPokemon(pokemon);
-    console.log(index + 1, pokemon);
-  });
-};
-
-const fetchPokemon = async (pokemon) => {
-  let url = pokemon.url;
-  const response = await fetch(url);
-  const pokemonData = await response.json();
-  console.log(pokemonData);
-};
 
 const GameWindow = () => {
   const [curr, setCurr] = useState(0);
   const [best, setBest] = useState(0);
+  const [pokeArr, setPokeArr] = useState([]);
 
-  fetchOgPokemon();
+  useEffect(() => {
+    let ignore = false;
+
+    const fetchOgPokemon = async () => {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=151",
+      );
+      const ogPokemon = await response.json();
+      //console.log(ogPokemon);
+      setPokeArr(
+        ogPokemon.results.map((pokemon, index) => {
+          const id = index + 1;
+          const name = pokemon.name;
+          const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+          return { id, name, img };
+          // console.log(index + 1, pokemon);
+        }),
+      );
+    };
+
+    fetchOgPokemon();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
 
   return (
     <main>
       <p>Best Score: {curr}</p>
       <p>Current Score: {best}</p>
+      <p>Possible pokemon: {pokeArr.length}</p>
     </main>
   );
 };
