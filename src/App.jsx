03d8c529a796355d2b14ec5aa95b7764d0eb7aa5
ShrikeSwallow <maciej.jaskolka@gmail.com>
2025-01-21 04:45:02 +0000

    Change copy in the main App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <>
       <h1 className="text-4xl font-black">Pokemon Memory Game</h1>
+      <h2 className="font text-2xl font-semibold">
+        Get points by clicking on an image but don&apos;t click on any more than
+        once!
+      </h2>
     </>
   );
 }
