6a0518513d3c7b2b48afb6b91b2f4083b4bf403d
ShrikeSwallow <maciej.jaskolka@gmail.com>
2025-01-22 06:22:01 +0000

    Add function to choose random pokemon from the fetched array

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // import { useState } from "react";
+import { Fragment } from "react";
 import GameWindow from "./components/GameWindow";
 
 function App() {
   return (
-    <>
+    <Fragment>
       <h1 className="text-4xl font-black">Pokemon Memory Game</h1>
       <h2 className="font text-2xl font-semibold">
         Get points by clicking on an image but don&apos;t click on any more than
         once!
       </h2>
       <GameWindow></GameWindow>
-    </>
+    </Fragment>
   );
 }
 
--- src/components/GameWindow.jsx
+++ src/components/GameWindow.jsx
@@ -6,39 +6,63 @@ const GameWindow = () => {
   const [curr, setCurr] = useState(0);
   const [best, setBest] = useState(0);
   const [pokeArr, setPokeArr] = useState([]);
+  const [cardsList, setCardsList] = useState([]);
+  const [game, setGame] = useState("pre");
 
   useEffect(() => {
+    /* get data from external API */
     let ignore = false;
 
-    const fetchOgPokemon = async () => {
-      const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=151",
-      );
-      const ogPokemon = await response.json();
-      //console.log(ogPokemon);
-      setPokeArr(
-        ogPokemon.results.map((pokemon, index) => {
-          const id = index + 1;
-          const name = pokemon.name;
-          const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-          return { id, name, img };
-          // console.log(index + 1, pokemon);
-        }),
-      );
-    };
-
-    fetchOgPokemon();
+    if (!ignore) {
+      const fetchOgPokemon = async () => {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=151",
+        );
+        const ogPokemon = await response.json();
+        setPokeArr(
+          ogPokemon.results.map((pokemon, index) => {
+            const id = index + 1;
+            const name = pokemon.name;
+            const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+            return { id, name, img };
+          }),
+        );
+      };
+      fetchOgPokemon();
+    }
 
     return () => {
       ignore = true;
     };
   }, []);
 
+  const newGame = () => {
+    setCurr(0);
+    setGame("active");
+    const cardsSet = new Set();
+    const limit = 4;
+    while (cardsSet.size < limit) {
+      cardsSet.add(Math.floor(Math.random() * pokeArr.length));
+    }
+    setCardsList([...cardsSet]);
+  };
+
   return (
     <main>
       <p>Best Score: {curr}</p>
       <p>Current Score: {best}</p>
       <p>Possible pokemon: {pokeArr.length}</p>
+      <p>Current game size: {cardsList.length}</p>
+      <button onClick={newGame} className="rounded bg-slate-400 p-2">
+        Start new game
+      </button>
+      {game === "active" && (
+        <ul>
+          {cardsList.map((card) => (
+            <li key={card}>{card}</li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 };
